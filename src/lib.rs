08b0ac1589e072e235fd6011e98b96dff9947f26
Sophie Willer <sophiewiller93@gmail.com>
2025-11-08 00:23:38 -0800

    parse input as ItemEnum

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,10 @@ use proc_macro::TokenStream;
 use quote::quote;
 
 #[proc_macro_derive(EnumVariants)]
-pub fn variants_derive(input: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
-    let name = input.ident;
-
-    let variants = match input.data {
-        syn::Data::Enum(data_enum) => data_enum.variants.into_iter().map(|v| v.ident),
-        _ => panic!("EnumVariants only works on Enums, champ..."),
-    };
+pub fn enum_variants(input: TokenStream) -> TokenStream {
+    let input: syn::ItemEnum = syn::parse_macro_input!(input);
+    let name = &input.ident;
+    let variants = input.variants.into_iter().map(|v| v.ident);
 
     let expanded = quote! {
         impl #name {
